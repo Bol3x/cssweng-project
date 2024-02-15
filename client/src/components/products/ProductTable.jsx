83e6fb2f844f25c36d66b9a1791f7f6c4046f975
Carlo Roleda <carloroleda@gmail.com>
2024-02-15 18:43:08 +0800

    Removed client folder
    deprecated

--- client/src/components/products/ProductTable.jsx
+++
@@ -1,9 +0,0 @@
-import Table from '../table/Table';
-import { COLUMNS } from '../table/columns';
-
-//hook function
-function ProductTable({products, isFetching, setProductsToDelete}){
-    return <Table columns={COLUMNS} data={products} isFetching={isFetching} setSelectedRowData={setProductsToDelete}/>;
-}
-
-export default ProductTable;
