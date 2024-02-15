b36a4f20751ab72640bb66f5051a0978da652db1
AntonAlonzo <87627127+AntonAlonzo@users.noreply.github.com>
2024-02-15 11:18:39 +0800

    Update validatePhone.ts

--- server/src/services/validation/validatePhone.ts
+++ server/src/services/validation/validatePhone.ts
@@ -2,11 +2,24 @@
 // mainly checks input format of the number
 
 export default function validatePhone(number: string){
-	try{
-		//mobile number
 
-		//todo: check country code for phone number
-		if (!number.search(/^[0-9]{11}$/gm))
+	/*
+		REGEX LEGEND:
+
+		[1]		(?:\+?(\d{1,3}))?		country code		optional
+		[2]		(\d{3})?            	area code			optional
+		[3]		(\d{3})             	exchange code		required
+		[4]		(\d{4})             	subscriber number	required
+		[5]		(?: *x(\d+))?       	extension number	optional
+
+		Groups can be divided with either spaces or dashes
+	*/
+
+	const phoneRe = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})?[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/gm;
+
+	try{
+
+		if (!number.search(phoneRe))
 			return true;
 	}
 	catch(err){
