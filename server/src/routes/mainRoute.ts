9793f4254820916474f56a1a113d6566caddc195
AntonAlonzo <87627127+AntonAlonzo@users.noreply.github.com>
2024-03-17 23:10:11 +0800

    it in fact did not work
    window is not defined error. im gonna try another implementation

--- server/src/routes/mainRoute.ts
+++ server/src/routes/mainRoute.ts
@@ -14,8 +14,6 @@ import ErrorHandler from '../services/error/errorResponse.js';
 import userCheckAuth from '../services/user/auth/userCheckAuth.js';
 import userCheckAdmin from '../services/user/auth/userCheckAdmin.js';
 
-import { sessionTimer } from '../services/middlewares/sessionTimeout.js'
-
 export const LoadRoutes = (app: Router) => {
 
 	app.use('/product', userCheckAuth, ProductRouter);
@@ -28,14 +26,11 @@ export const LoadRoutes = (app: Router) => {
 	//main route to react app
 	app.get('/', (req, res) => {
 		if (req.isAuthenticated()){
-			sessionTimer.reset();
 			//@ts-ignore
 			res.render('index', {name:  req.user.name});
 		}
-		else{
-			sessionTimer.dispose();
+		else
 			res.render('login')
-		}
 	})
 
 	//redirect all other routes to index
